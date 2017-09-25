95695c706fba2b050aa2e7de79a2fda9be40e9c1
Nickbing Lao <giscafer@outlook.com>
2017-09-25 15:30:53 +0800

    support dynamic render img data
    support dynamic render img data

--- viewer.directive.ts
+++ viewer.directive.ts
@@ -3,41 +3,67 @@
  * 基于viewer.js封装
  */
 import {
+    ContentChild,
+    ContentChildren,
     Directive,
     ElementRef,
-    Renderer,
     Input,
-    Output,
-    HostListener,
-    OnChanges,
+    NgModule,
     OnDestroy,
-    SimpleChange,
-    EventEmitter, OnInit, NgModule, SimpleChanges, Renderer2, AfterViewInit, ContentChild
+    OnInit,
+    Renderer2
 } from "@angular/core";
-import { Http } from "@angular/http";
-import { CommonModule } from "@angular/common";
-import { FormsModule } from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 declare var Viewer;
+
+
+@Directive({
+    selector: '[viewer-content]',
+})
+export class ViewerContentDirective {
+
+    nativeElement: HTMLElement;
+
+    constructor(private _el: ElementRef) {
+        this.nativeElement = this._el.nativeElement;
+    }
+
+}
 
 @Directive({
     selector: '[viewer]'
 })
 export class ViewerDirective implements OnInit, OnDestroy {
     @Input()
-    originalAttr: string = "data-original";
+    originalAttr: string = "name";
     @ContentChild('content') content: ElementRef;
 
+    @ContentChildren(ViewerContentDirective)
+    set _imgContents(value) {
+        this.imgContents = value;
+        this.renderContent(true);
+    }
+
     viewer: any;
+    imgContents;
     nativeElement: HTMLElement;
 
     constructor(private _elementRef: ElementRef,
-        private _render: Renderer2) {
+                private _render: Renderer2) {
         this.nativeElement = this._elementRef.nativeElement;
 
     }
-    
+
     ngOnInit() {
+        this.renderContent();
+    }
+
+    /**
+     * 如果img动态增加，则动态渲染
+     * @param {boolean} flag 是否动态增加渲染
+     */
+    renderContent(flag?: boolean) {
         let nativeEl = this.nativeElement;
         if (this.content) {
             nativeEl = this.content.nativeElement;
@@ -45,11 +71,23 @@ export class ViewerDirective implements OnInit, OnDestroy {
         setTimeout(() => {
             let imgs = nativeEl.getElementsByTagName('img');
             if (imgs.length) {
-                imgs[0].onload = () => {
-                    this.viewer = new Viewer(this.nativeElement, {
-                        url: this.originalAttr,
-                    });
+                if (this.viewer) {
+                    this.viewer.destroy();
                 }
+                if (!flag) {
+                    imgs[0].onload = () => {
+                        this.viewer = new Viewer(this.nativeElement, {
+                            url: this.originalAttr,
+                        });
+                    }
+                } else {
+                    imgs[imgs.length-1].onload = () => {
+                        this.viewer = new Viewer(this.nativeElement, {
+                            url: this.originalAttr,
+                        });
+                    }
+                }
+
             }
         });
     }
@@ -59,4 +97,16 @@ export class ViewerDirective implements OnInit, OnDestroy {
             this.viewer.destroy();
         }
     }
+}
+
+@NgModule({
+    declarations: [
+        ViewerDirective, ViewerContentDirective
+    ],
+    exports: [ViewerDirective, ViewerContentDirective],
+    imports: [
+        CommonModule
+    ]
+})
+export class ViewerDirectiveModule {
 }
